54f12ede7d923d33fca28ca0dd816a5bdd24cfc6
Stronger <114509657+king-stronger@users.noreply.github.com>
2024-07-02 19:25:56 +0000

    Implement Calculator Functions and Button Functionality
    - Implemented add, subtract, multiply, and divide
      functions for calculator operations.
    - Created the operate function to handle operations
      based on the selected operator and operands.
    - Implemented the del function to clear the last input.
    - Implemented the reset function to clear all inputs
      and results.
    - Linked calculator buttons to their respective
      JavaScript functions for interactive functionality.

---
+++ assets/js/main.js
@@ -0,0 +1,152 @@
+let result;
+let float=false;
+let display = ""; 
+let operator = "";
+let firstNumber = "";
+let secondNumber = "";
+
+const delButton = document.querySelector(".del");
+const floatButton = document.querySelector(".float");
+const resetButton = document.querySelector(".reset");
+const displayBox = document.querySelector(".display");
+const resultButton = document.querySelector(".equal");
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operators");
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(firstNumber && !secondNumber){
+            operator = button.getAttribute("data-value");
+        } else {
+            operate(operator, firstNumber, secondNumber);
+            firstNumber = `${result}`, secondNumber = "";
+            operator = button.getAttribute("data-value");
+        }
+
+        displayContent();
+    });
+});
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(operator){
+            secondNumber += button.getAttribute("data-value");
+        } else {
+            firstNumber += button.getAttribute("data-value");
+        }
+
+        displayContent();
+    })
+});
+
+resultButton.addEventListener("click", () => {
+    operate(operator, firstNumber, secondNumber);
+    displayContent(result);
+});
+
+delButton.addEventListener("click", () => {
+    if(operator){
+        secondNumber = del(secondNumber);
+    } else {
+        firstNumber = del(firstNumber);
+    }
+
+    displayContent();
+});
+
+resetButton.addEventListener("click", () => {
+    reset();
+});
+
+floatButton.addEventListener("click", () => {
+    if(!operator && !firstNumber.includes(".")){
+        firstNumber += floatButton.getAttribute("data-value");
+        float = true;
+    } else if(operator && !secondNumber.includes(".")){
+        secondNumber += floatButton.getAttribute("data-value");
+    }
+
+    return displayContent();
+});
+
+
+function add (a, b){
+    return a + b;
+}
+
+function substract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if(b == "0"){
+        alert("You can't divide a number by 0")
+        return reset();
+    }
+    return a / b;
+}
+
+function operate(symbol, firstValue, secondValue){
+    if(!firstValue || !secondValue){
+        alert("You need to provide two numbers");
+        return reset();
+    }
+
+    firstValue = parseFloat(firstValue);
+    secondValue = parseFloat(secondValue);
+
+    switch(symbol){
+        case "+":
+            result = add(firstValue, secondValue);
+            break
+        case "-":
+            result = substract(firstValue, secondValue);
+            break;
+        case "*":
+            result = multiply(firstValue, secondValue);
+            break;
+        case "/":
+            result = divide(firstValue, secondValue);
+            break;
+        default:
+            break;
+    }
+
+    if(!float){
+        result = parseInt(result);
+    } else {
+        result = Math.round(result * 100) / 100;
+    }
+}
+
+function displayContent(content){
+    if(content){
+        displayBox.textContent = content;
+
+        return reset();
+    }
+
+    display = `${firstNumber} ${operator} ${secondNumber}`;
+
+    displayBox.textContent = display.trim();
+}
+
+function del(number){
+    let length = number.length;
+    return number.slice(0, length - 1);
+}
+
+
+function reset(){
+    displayBox.textContent = result;
+    display = "";
+    operator = "";
+    result = null;
+    float = false;
+    firstNumber = "";
+    secondNumber = ""; 
+}
